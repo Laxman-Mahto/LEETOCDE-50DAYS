714d170c2f1e35bcc9648deecedd3264a1a91160
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-14 18:52:00 +0530

    test(day01): add test cases and main runner for a217

--- src/day01/a217.java
+++ src/day01/a217.java
@@ -4,6 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class a217 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(s.containsDuplicate(nums));
+    }
 
     static class Solution {
         public boolean containsDuplicate(int[] nums) {
