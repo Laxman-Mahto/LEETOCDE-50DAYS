b29f38ff405769e727d4a1a54ad3b3b0dbb7812a
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-14 18:42:00 +0530

    feat(day01): solve LC 217 - Contains Duplicate

---
+++ src/day01/a217.java
@@ -0,0 +1,19 @@
+package day01;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class a217 {
+
+    static class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                if (!set.add(num)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
