395ea63f7fe3ecd09da6a90602a36523c694f73c
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-15 19:36:00 +0530

    test(day02): add test cases and main runner for c27

--- src/day02/c27.java
+++ src/day02/c27.java
@@ -1,6 +1,11 @@
 package day02;
 
 public class c27 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {3, 2, 2, 3};
+        System.out.println(s.removeElement(nums, 3));
+    }
 
     static class Solution {
         public int removeElement(int[] nums, int val) {
