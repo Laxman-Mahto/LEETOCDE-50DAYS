78a7dfb85f86a317662a271557189eeb009ec3cc
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-16 19:14:00 +0530

    test(day03): add test cases and main runner for b14

--- src/day03/b14.java
+++ src/day03/b14.java
@@ -1,6 +1,11 @@
 package day03;
 
 public class b14 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(s.longestCommonPrefix(strs));
+    }
 
     static class Solution {
         public String longestCommonPrefix(String[] strs) {
