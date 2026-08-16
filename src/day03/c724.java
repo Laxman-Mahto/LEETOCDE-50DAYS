2e8b765fd8cedd1f311cb496f79a03dcee53a154
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-16 19:36:00 +0530

    test(day03): add test cases and main runner for c724

--- src/day03/c724.java
+++ src/day03/c724.java
@@ -1,6 +1,11 @@
 package day03;
 
 public class c724 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 7, 3, 6, 5, 6};
+        System.out.println(s.pivotIndex(nums));
+    }
 
     static class Solution {
         public int pivotIndex(int[] nums) {
