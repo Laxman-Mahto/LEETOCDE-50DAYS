8221be5e8754bb6fbdec3a51d59867d2da9b8817
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-17 18:42:00 +0530

    feat(day04): solve LC 167 - Two Sum II - Input Array Is Sorted

---
+++ src/day04/a167.java
@@ -0,0 +1,24 @@
+package day04;
+
+import java.util.Arrays;
+
+public class a167 {
+
+    static class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int left = 0;
+            int right = numbers.length - 1;
+            while (left < right) {
+                int sum = numbers[left] + numbers[right];
+                if (sum == target) {
+                    return new int[]{left + 1, right + 1};
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            return new int[]{-1, -1};
+        }
+    }
+}
