6d2fd94a70624a24ee952c54f6d505765b3a7b84
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-17 18:52:00 +0530

    test(day04): add test cases and main runner for a167

--- src/day04/a167.java
+++ src/day04/a167.java
@@ -3,6 +3,11 @@ package day04;
 import java.util.Arrays;
 
 public class a167 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] numbers = {2, 7, 11, 15};
+        System.out.println(Arrays.toString(s.twoSum(numbers, 9)));
+    }
 
     static class Solution {
         public int[] twoSum(int[] numbers, int target) {
