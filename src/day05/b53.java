a129877d4a4bfe4000323b7de6cedc70a3cda2b9
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-18 19:14:00 +0530

    test(day05): add test cases and main runner for b53

--- src/day05/b53.java
+++ src/day05/b53.java
@@ -1,6 +1,11 @@
 package day05;
 
 public class b53 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(s.maxSubArray(nums));
+    }
 
     static class Solution {
         public int maxSubArray(int[] nums) {
