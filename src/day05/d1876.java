e825bc9e9f4bfe8ce7ebc3abd25f7b18ab15cd7b
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-18 19:48:00 +0530

    feat(day05): solve LC 1876 - Substrings of Size Three with Distinct Characters

---
+++ src/day05/d1876.java
@@ -0,0 +1,19 @@
+package day05;
+
+public class d1876 {
+
+    static class Solution {
+        public int countGoodSubstrings(String s) {
+            int count = 0;
+            for (int i = 0; i + 2 < s.length(); i++) {
+                char a = s.charAt(i);
+                char b = s.charAt(i + 1);
+                char c = s.charAt(i + 2);
+                if (a != b && b != c && a != c) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
