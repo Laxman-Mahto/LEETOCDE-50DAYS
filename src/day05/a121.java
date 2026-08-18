d933a2554d6cac8d69ff6360a80980c6517ae8e4
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-18 18:52:00 +0530

    test(day05): add test cases and main runner for a121

--- src/day05/a121.java
+++ src/day05/a121.java
@@ -1,6 +1,11 @@
 package day05;
 
 public class a121 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(s.maxProfit(prices));
+    }
 
     static class Solution {
         public int maxProfit(int[] prices) {
