1cd63e848ad353772aa38e374a59aa32db701702
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-19 19:14:00 +0530

    test(Day06): add test cases and main runner for b150

--- src/Day06/b150.java
+++ src/Day06/b150.java
@@ -3,6 +3,11 @@ package Day06;
 import java.util.Stack;
 
 public class b150 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] tokens = {"2", "1", "+", "3", "*"};
+        System.out.println(s.evalRPN(tokens));
+    }
     static class Solution {
         public int evalRPN(String[] tokens) {
             Stack<Integer> stack = new Stack<>();
