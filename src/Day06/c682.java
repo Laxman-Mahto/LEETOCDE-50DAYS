0f35a65c8e6f420bddd6c12b2fb303d35cfe9e5f
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-19 19:26:00 +0530

    feat(Day06): solve LC 682 - Baseball Game

---
+++ src/Day06/c682.java
@@ -0,0 +1,28 @@
+package Day06;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class c682 {
+    static class Solution {
+        public int calPoints(String[] operations) {
+            List<Integer> record = new ArrayList<>();
+            for (String op : operations) {
+                if (op.equals("+")) {
+                    record.add(record.get(record.size() - 1) + record.get(record.size() - 2));
+                } else if (op.equals("D")) {
+                    record.add(record.get(record.size() - 1) * 2);
+                } else if (op.equals("C")) {
+                    record.remove(record.size() - 1);
+                } else {
+                    record.add(Integer.parseInt(op));
+                }
+            }
+            int total = 0;
+            for (int score : record) {
+                total += score;
+            }
+            return total;
+        }
+    }
+}
