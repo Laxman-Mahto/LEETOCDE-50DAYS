9ae2b478af219ca3d04d27173ca8db03c36f2a73
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-19 18:42:00 +0530

    feat(Day06): solve LC 155 - Min Stack

---
+++ src/Day06/a155.java
@@ -0,0 +1,36 @@
+package Day06;
+
+import java.util.Stack;
+
+public class a155 {
+    static class MinStack {
+        private Stack<Integer> stack;
+        private Stack<Integer> minStack;
+
+        public MinStack() {
+            stack = new Stack<>();
+            minStack = new Stack<>();
+        }
+
+        public void push(int val) {
+            stack.push(val);
+            if (minStack.isEmpty() || val <= minStack.peek()) {
+                minStack.push(val);
+            }
+        }
+
+        public void pop() {
+            if (stack.pop().equals(minStack.peek())) {
+                minStack.pop();
+            }
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+}
