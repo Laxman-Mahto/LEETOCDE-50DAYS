a4b86a32ed87cdc10261b2940d53213f11c78b43
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-20 19:36:00 +0530

    test(Day07): add test cases and main runner for c74

--- src/Day07/c74.java
+++ src/Day07/c74.java
@@ -1,6 +1,11 @@
 package Day07;
 
 public class c74 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+        System.out.println(s.searchMatrix(matrix, 3));
+    }
     static class Solution {
         public boolean searchMatrix(int[][] matrix, int target) {
             int m = matrix.length;
