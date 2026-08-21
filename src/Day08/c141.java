7c7f46be99ee4f1e85d562b6cf4fdc2e2f64bb8a
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-21 19:26:00 +0530

    feat(Day08): solve LC 141 - Linked List Cycle

---
+++ src/Day08/c141.java
@@ -0,0 +1,29 @@
+package Day08;
+
+public class c141 {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static class Solution {
+        public boolean hasCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while (slow != fast) {
+                if (fast == null || fast.next == null) {
+                    return false;
+                }
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
+        }
+    }
+}
