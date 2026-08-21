34932040c38f18958c8c2c8aacf4ff2d8c5de3d9
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-21 19:58:00 +0530

    test(Day08): add test cases and main runner for d876

--- src/Day08/d876.java
+++ src/Day08/d876.java
@@ -1,6 +1,14 @@
 package Day08;
 
 public class d876 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        ListNode res = s.middleNode(head);
+        System.out.println(res.val);
+    }
     static class ListNode {
         int val;
         ListNode next;
