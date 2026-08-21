02a67f7183ec67e4e1ace1b14d1ad0b1ccb3b3bf
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-21 19:48:00 +0530

    feat(Day08): solve LC 876 - Middle of the Linked List

---
+++ src/Day08/d876.java
@@ -0,0 +1,23 @@
+package Day08;
+
+public class d876 {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+}
