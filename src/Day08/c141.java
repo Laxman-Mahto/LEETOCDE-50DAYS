1fab92b07894cbaae1fbe488c0fed397dcc3f4e6
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-21 19:36:00 +0530

    test(Day08): add test cases and main runner for c141

--- src/Day08/c141.java
+++ src/Day08/c141.java
@@ -1,6 +1,14 @@
 package Day08;
 
 public class c141 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = head.next;
+        System.out.println(s.hasCycle(head));
+    }
     static class ListNode {
         int val;
         ListNode next;
