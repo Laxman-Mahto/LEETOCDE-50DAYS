3419132f24a39f7184c01f5221010c3a9c20afa8
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-22 20:10:00 +0530

    feat(Day09): solve LC 328 - Odd Even Linked List

---
+++ src/Day09/e328.java
@@ -0,0 +1,30 @@
+package Day09;
+
+public class e328 {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static class Solution {
+        public ListNode oddEvenList(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+            ListNode odd = head;
+            ListNode even = head.next;
+            ListNode evenHead = even;
+            while (even != null && even.next != null) {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+            return head;
+        }
+    }
+}
