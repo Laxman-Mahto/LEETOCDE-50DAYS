9de18909912743ab03411feca5047d33970cd233
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-24 18:52:00 +0530

    test(Day11): add test cases and main runner for a15

--- src/Day11/a15.java
+++ src/Day11/a15.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class a15 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(s.threeSum(nums));
+    }
     static class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
             List<List<Integer>> result = new ArrayList<>();
