97a0ad747e97a22a2815d4ab39aba8aa34dd8621
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-24 19:14:00 +0530

    test(Day11): add test cases and main runner for b11

--- src/Day11/b11.java
+++ src/Day11/b11.java
@@ -1,6 +1,11 @@
 package Day11;
 
 public class b11 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(s.maxArea(height));
+    }
     static class Solution {
         public int maxArea(int[] height) {
             int left = 0;
