a3993e7945500c7002f825961ef36a4b533705a3
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-25 19:26:00 +0530

    feat(DAY12): solve LC 424 - Longest Repeating Character Replacement

---
+++ src/DAY12/c424.java
@@ -0,0 +1,22 @@
+package DAY12;
+
+public class c424 {
+    static class Solution {
+        public int characterReplacement(String s, int k) {
+            int[] count = new int[26];
+            int maxCount = 0;
+            int maxLen = 0;
+            int left = 0;
+            for (int right = 0; right < s.length(); right++) {
+                count[s.charAt(right) - 'A']++;
+                maxCount = Math.max(maxCount, count[s.charAt(right) - 'A']);
+                while ((right - left + 1) - maxCount > k) {
+                    count[s.charAt(left) - 'A']--;
+                    left++;
+                }
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            return maxLen;
+        }
+    }
+}
