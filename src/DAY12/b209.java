562e27d263c00cf7ab96a0bde52a0b6605a57e29
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-25 19:14:00 +0530

    test(DAY12): add test cases and main runner for b209

--- src/DAY12/b209.java
+++ src/DAY12/b209.java
@@ -1,6 +1,11 @@
 package DAY12;
 
 public class b209 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        System.out.println(s.minSubArrayLen(7, nums));
+    }
     static class Solution {
         public int minSubArrayLen(int target, int[] nums) {
             int minLen = Integer.MAX_VALUE;
