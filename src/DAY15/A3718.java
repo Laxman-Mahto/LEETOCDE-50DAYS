abc65036ce0d656462f845e7f771b070fb2cebdb
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-25 23:28:08 +0530

    day 15

---
+++ src/DAY15/A3718.java
@@ -0,0 +1,41 @@
+package DAY15;
+
+public class A3718 {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] nums = {8, 2, 3, 4, 6};
+        int k = 2;
+
+        int answer = solution.missingMultiple(nums, k);
+
+        System.out.println("Answer: " + answer);
+    }
+
+    static class Solution {
+
+        public int missingMultiple(int[] nums, int k) {
+
+            boolean[] present = new boolean[101];
+
+            for (int num : nums) {
+                present[num] = true;
+            }
+
+            int multiple = k;
+
+            while (multiple <= 100) {
+
+                if (!present[multiple]) {
+                    return multiple;
+                }
+
+                multiple += k;
+            }
+
+            return multiple;
+        }
+    }
+}m
