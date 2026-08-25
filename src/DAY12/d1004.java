6ce22019ca37286ddfbbb5e2ebee2558de2bca2d
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-25 19:58:00 +0530

    test(DAY12): add test cases and main runner for d1004

--- src/DAY12/d1004.java
+++ src/DAY12/d1004.java
@@ -1,6 +1,11 @@
 package DAY12;
 
 public class d1004 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
+        System.out.println(s.longestOnes(nums, 2));
+    }
     static class Solution {
         public int longestOnes(int[] nums, int k) {
             int left = 0;
