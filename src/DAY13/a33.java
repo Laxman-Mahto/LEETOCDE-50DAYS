02f1d9296a707f731e011987380e981a76864704
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-26 18:52:00 +0530

    test(DAY13): add test cases and main runner for a33

--- src/DAY13/a33.java
+++ src/DAY13/a33.java
@@ -1,6 +1,11 @@
 package DAY13;
 
 public class a33 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(s.search(nums, 0));
+    }
     static class Solution {
         public int search(int[] nums, int target) {
             int left = 0;
