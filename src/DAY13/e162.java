26973688c51cf6b17ca8402d85d8b08b3f1c8921
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-26 20:10:00 +0530

    feat(DAY13): solve LC 162 - Find Peak Element

---
+++ src/DAY13/e162.java
@@ -0,0 +1,19 @@
+package DAY13;
+
+public class e162 {
+    static class Solution {
+        public int findPeakElement(int[] nums) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > nums[mid + 1]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+    }
+}
