a4b6eaf06b2016913d011c3fb7daa539ea26a7a6
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-27 19:58:00 +0530

    test(DAY14): add test cases and main runner for d901

--- src/DAY14/d901.java
+++ src/DAY14/d901.java
@@ -3,6 +3,16 @@ package DAY14;
 import java.util.Stack;
 
 public class d901 {
+    public static void main(String[] args) {
+        StockSpanner spanner = new StockSpanner();
+        System.out.println(spanner.next(100));
+        System.out.println(spanner.next(80));
+        System.out.println(spanner.next(60));
+        System.out.println(spanner.next(70));
+        System.out.println(spanner.next(60));
+        System.out.println(spanner.next(75));
+        System.out.println(spanner.next(85));
+    }
     static class StockSpanner {
         private Stack<int[]> stack;
 
