3cdb18ed27e32c71f62772a418192497946e89fd
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-27 19:36:00 +0530

    test(DAY14): add test cases and main runner for c503

--- src/DAY14/c503.java
+++ src/DAY14/c503.java
@@ -4,6 +4,12 @@ import java.util.Arrays;
 import java.util.Stack;
 
 public class c503 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 2, 1};
+        int[] res = s.nextGreaterElements(nums);
+        System.out.println(java.util.Arrays.toString(res));
+    }
     static class Solution {
         public int[] nextGreaterElements(int[] nums) {
             int n = nums.length;
