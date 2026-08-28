9696ed32148566e0357d7724eb810e31f885994f
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-28 18:52:00 +0530

    test(DAY15): add test cases and main runner for a226

--- src/DAY15/a226.java
+++ src/DAY15/a226.java
@@ -1,6 +1,14 @@
 package DAY15;
 
 public class a226 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        TreeNode res = s.invertTree(root);
+        System.out.println(res.val);
+    }
     static class TreeNode {
         int val;
         TreeNode left;
