6c14a6a15d4238c6ccd3cc333b3861afe68a65ac
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-28 19:04:00 +0530

    feat(DAY15): solve LC 100 - Same Tree

---
+++ src/DAY15/b100.java
@@ -0,0 +1,24 @@
+package DAY15;
+
+public class b100 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static class Solution {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p == null || q == null || p.val != q.val) {
+                return false;
+            }
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
