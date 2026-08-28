f19f03176c041c3f20525e1b11c03ab6409d08a6
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-28 19:14:00 +0530

    test(DAY15): add test cases and main runner for b100

--- src/DAY15/b100.java
+++ src/DAY15/b100.java
@@ -1,6 +1,12 @@
 package DAY15;
 
 public class b100 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode p = new TreeNode(1);
+        TreeNode q = new TreeNode(1);
+        System.out.println(s.isSameTree(p, q));
+    }
     static class TreeNode {
         int val;
         TreeNode left;
