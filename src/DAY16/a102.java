70a4c9a78b826b3d7895ff7e11ef6fc359a9e584
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-29 18:52:00 +0530

    test(DAY16): add test cases and main runner for a102

--- src/DAY16/a102.java
+++ src/DAY16/a102.java
@@ -6,6 +6,13 @@ import java.util.List;
 import java.util.Queue;
 
 public class a102 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        System.out.println(s.levelOrder(root));
+    }
     static class TreeNode {
         int val;
         TreeNode left;
