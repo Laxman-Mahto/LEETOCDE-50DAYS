8809d2316d18b269041601e19ca1daea36d4e5b3
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-29 19:36:00 +0530

    test(DAY16): add test cases and main runner for c199

--- src/DAY16/c199.java
+++ src/DAY16/c199.java
@@ -6,6 +6,13 @@ import java.util.List;
 import java.util.Queue;
 
 public class c199 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        System.out.println(s.rightSideView(root));
+    }
     static class TreeNode {
         int val;
         TreeNode left;
