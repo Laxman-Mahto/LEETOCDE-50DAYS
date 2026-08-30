15f8d1edf5dbafdc58147f595bcb0b65018e5f60
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-30 19:14:00 +0530

    test(day17): add test cases and main runner for b113

--- src/day17/b113.java
+++ src/day17/b113.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class b113 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        System.out.println(s.pathSum(root, 9));
+    }
     static class TreeNode {
         int val;
         TreeNode left;
