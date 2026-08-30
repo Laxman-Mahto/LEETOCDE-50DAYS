17c6572e126be143c07164cbb273867cd2925afb
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-30 19:26:00 +0530

    feat(day17): solve LC 236 - Lowest Common Ancestor of a Binary Tree

---
+++ src/day17/c236.java
@@ -0,0 +1,26 @@
+package day17;
+
+public class c236 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null || root == p || root == q) {
+                return root;
+            }
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if (left != null && right != null) {
+                return root;
+            }
+            return left != null ? left : right;
+        }
+    }
+}
