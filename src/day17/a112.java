67d50c532880b2f8131883c44ba8eab65e5bd95e
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-30 18:42:00 +0530

    feat(day17): solve LC 112 - Path Sum

---
+++ src/day17/a112.java
@@ -0,0 +1,25 @@
+package day17;
+
+public class a112 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static class Solution {
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            if (root == null) {
+                return false;
+            }
+            if (root.left == null && root.right == null) {
+                return targetSum == root.val;
+            }
+            return hasPathSum(root.left, targetSum - root.val) ||
+                   hasPathSum(root.right, targetSum - root.val);
+        }
+    }
+}
