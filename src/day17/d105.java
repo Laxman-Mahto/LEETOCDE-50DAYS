ae83267b61741f1db8087a76a29d25305cbc3c8b
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-30 19:58:00 +0530

    test(day17): add test cases and main runner for d105

--- src/day17/d105.java
+++ src/day17/d105.java
@@ -4,6 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class d105 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        TreeNode res = s.buildTree(preorder, inorder);
+        System.out.println(res.val);
+    }
     static class TreeNode {
         int val;
         TreeNode left;
