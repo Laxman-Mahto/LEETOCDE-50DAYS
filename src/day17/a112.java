e3f20d55fa520250a2d4529a60965d5318ed63aa
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-30 18:52:00 +0530

    test(day17): add test cases and main runner for a112

--- src/day17/a112.java
+++ src/day17/a112.java
@@ -1,6 +1,13 @@
 package day17;
 
 public class a112 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        System.out.println(s.hasPathSum(root, 9));
+    }
     static class TreeNode {
         int val;
         TreeNode left;
