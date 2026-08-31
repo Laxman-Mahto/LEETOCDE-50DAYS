0ba420e9575e886c46814108dcbf3a8b6b5c3794
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-31 20:10:00 +0530

    feat(day18): solve LC 235 - Lowest Common Ancestor of a Binary Search Tree

---
+++ src/day18/e235.java
@@ -0,0 +1,27 @@
+package day18;
+
+public class e235 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) {
+                return null;
+            }
+            if (p.val < root.val && q.val < root.val) {
+                return lowestCommonAncestor(root.left, p, q);
+            }
+            if (p.val > root.val && q.val > root.val) {
+                return lowestCommonAncestor(root.right, p, q);
+            }
+            return root;
+        }
+    }
+}
