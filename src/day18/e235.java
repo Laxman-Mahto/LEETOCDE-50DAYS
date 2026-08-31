61f3184d5b4905d2622ffed75abe7d1d5dc8a450
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-31 20:20:00 +0530

    test(day18): add test cases and main runner for e235

--- src/day18/e235.java
+++ src/day18/e235.java
@@ -1,6 +1,16 @@
 package day18;
 
 public class e235 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(6);
+        TreeNode p = new TreeNode(2);
+        TreeNode q = new TreeNode(8);
+        root.left = p;
+        root.right = q;
+        TreeNode res = s.lowestCommonAncestor(root, p, q);
+        System.out.println(res.val);
+    }
     static class TreeNode {
         int val;
         TreeNode left;
