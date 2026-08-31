afd3e72f69ebcd41ee58a8da57baa3b72a6d4d3c
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-08-31 19:36:00 +0530

    test(day18): add test cases and main runner for c98

--- src/day18/c98.java
+++ src/day18/c98.java
@@ -1,6 +1,13 @@
 package day18;
 
 public class c98 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        System.out.println(s.isValidBST(root));
+    }
     static class TreeNode {
         int val;
         TreeNode left;
