3c8af026cc48dc68aa74453e9f22d2f27606511e
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-01 19:36:00 +0530

    test(day19): add test cases and main runner for c46

--- src/day19/c46.java
+++ src/day19/c46.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class c46 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 2, 3};
+        System.out.println(s.permute(nums));
+    }
     static class Solution {
         public List<List<Integer>> permute(int[] nums) {
             List<List<Integer>> result = new ArrayList<>();
