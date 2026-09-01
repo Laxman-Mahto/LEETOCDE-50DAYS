1e593b4f7640178f8802b0e35a699fd079ae8326
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-01 18:52:00 +0530

    test(day19): add test cases and main runner for a78

--- src/day19/a78.java
+++ src/day19/a78.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class a78 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 2, 3};
+        System.out.println(s.subsets(nums));
+    }
     static class Solution {
         public List<List<Integer>> subsets(int[] nums) {
             List<List<Integer>> result = new ArrayList<>();
