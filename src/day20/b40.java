840fe089b77aa13baa4eb957c0483ae09fffb9ff
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-02 19:14:00 +0530

    test(day20): add test cases and main runner for b40

--- src/day20/b40.java
+++ src/day20/b40.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class b40 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] candidates = {10, 1, 2, 7, 6, 1, 5};
+        System.out.println(s.combinationSum2(candidates, 8));
+    }
     static class Solution {
         public List<List<Integer>> combinationSum2(int[] candidates, int target) {
             List<List<Integer>> result = new ArrayList<>();
