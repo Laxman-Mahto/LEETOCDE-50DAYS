a0fd90c999c5685a6163dd50657019391658af1b
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-03 19:36:00 +0530

    test(day21): add test cases and main runner for c703

--- src/day21/c703.java
+++ src/day21/c703.java
@@ -3,6 +3,13 @@ package day21;
 import java.util.PriorityQueue;
 
 public class c703 {
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 8, 2};
+        KthLargest kth = new KthLargest(3, nums);
+        System.out.println(kth.add(3));
+        System.out.println(kth.add(5));
+        System.out.println(kth.add(10));
+    }
     static class KthLargest {
         private PriorityQueue<Integer> minHeap;
         private int k;
