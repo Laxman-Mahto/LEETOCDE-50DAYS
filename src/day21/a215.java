bd14d4ddfb2b52c8031583f914c9c9aa83a66c8c
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-03 18:52:00 +0530

    test(day21): add test cases and main runner for a215

--- src/day21/a215.java
+++ src/day21/a215.java
@@ -3,6 +3,11 @@ package day21;
 import java.util.PriorityQueue;
 
 public class a215 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        System.out.println(s.findKthLargest(nums, 2));
+    }
     static class Solution {
         public int findKthLargest(int[] nums, int k) {
             PriorityQueue<Integer> minHeap = new PriorityQueue<>();
