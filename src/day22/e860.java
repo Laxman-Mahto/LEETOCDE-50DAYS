2c69e59966654d23cd2e0fcd80859e50c913b526
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-04 20:10:00 +0530

    feat(day22): solve LC 860 - Lemonade Change

---
+++ src/day22/e860.java
@@ -0,0 +1,31 @@
+package day22;
+
+public class e860 {
+    static class Solution {
+        public boolean lemonadeChange(int[] bills) {
+            int five = 0;
+            int ten = 0;
+            for (int bill : bills) {
+                if (bill == 5) {
+                    five++;
+                } else if (bill == 10) {
+                    if (five == 0) {
+                        return false;
+                    }
+                    five--;
+                    ten++;
+                } else {
+                    if (ten > 0 && five > 0) {
+                        ten--;
+                        five--;
+                    } else if (five >= 3) {
+                        five -= 3;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
