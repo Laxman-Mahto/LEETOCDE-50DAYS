dd5f42d732fe3e41b2eeb9600fa930bb48dfd5d1
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-04 19:36:00 +0530

    test(day22): add test cases and main runner for c605

--- src/day22/c605.java
+++ src/day22/c605.java
@@ -1,6 +1,11 @@
 package day22;
 
 public class c605 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] flowerbed = {1, 0, 0, 0, 1};
+        System.out.println(s.canPlaceFlowers(flowerbed, 1));
+    }
     static class Solution {
         public boolean canPlaceFlowers(int[] flowerbed, int n) {
             int count = 0;
