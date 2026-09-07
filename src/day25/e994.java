59b43cd90a838fcf05fa6404875a515872bb491c
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-07 20:20:00 +0530

    test(day25): add test cases and main runner for e994

--- src/day25/e994.java
+++ src/day25/e994.java
@@ -4,6 +4,11 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class e994 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[][] grid = {{2, 1, 1}, {1, 1, 0}, {0, 1, 1}};
+        System.out.println(s.orangesRotting(grid));
+    }
     static class Solution {
         public int orangesRotting(int[][] grid) {
             int m = grid.length;
