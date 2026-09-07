d322c53810a0a92433755a7e4cd9dae867e05298
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-07 19:04:00 +0530

    feat(day25): solve LC 210 - Course Schedule II

---
+++ src/day25/b210.java
@@ -0,0 +1,41 @@
+package day25;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class b210 {
+    static class Solution {
+        public int[] findOrder(int numCourses, int[][] prerequisites) {
+            List<List<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < numCourses; i++) {
+                adj.add(new ArrayList<>());
+            }
+            int[] inDegree = new int[numCourses];
+            for (int[] pre : prerequisites) {
+                adj.get(pre[1]).add(pre[0]);
+                inDegree[pre[0]]++;
+            }
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < numCourses; i++) {
+                if (inDegree[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+            int[] order = new int[numCourses];
+            int index = 0;
+            while (!queue.isEmpty()) {
+                int curr = queue.poll();
+                order[index++] = curr;
+                for (int next : adj.get(curr)) {
+                    inDegree[next]--;
+                    if (inDegree[next] == 0) {
+                        queue.offer(next);
+                    }
+                }
+            }
+            return index == numCourses ? order : new int[0];
+        }
+    }
+}
