b01d81a02365bcc8a1d3c93ac387b42190534fac
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-08 19:36:00 +0530

    test(day26): add test cases and main runner for c208

--- src/day26/c208.java
+++ src/day26/c208.java
@@ -1,6 +1,13 @@
 package day26;
 
 public class c208 {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));
+        System.out.println(trie.search("app"));
+        System.out.println(trie.startsWith("app"));
+    }
     static class Trie {
         private TrieNode root;
 
