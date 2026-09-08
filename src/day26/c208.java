8c7014d8c02417e5256a4f5d330cf4ca17360087
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-08 19:26:00 +0530

    feat(day26): solve LC 208 - Implement Trie (Prefix Tree)

---
+++ src/day26/c208.java
@@ -0,0 +1,49 @@
+package day26;
+
+public class c208 {
+    static class Trie {
+        private TrieNode root;
+
+        static class TrieNode {
+            TrieNode[] children = new TrieNode[26];
+            boolean isEnd;
+        }
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                if (node.children[c - 'a'] == null) {
+                    node.children[c - 'a'] = new TrieNode();
+                }
+                node = node.children[c - 'a'];
+            }
+            node.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            TrieNode node = root;
+            for (char c : word.toCharArray()) {
+                if (node.children[c - 'a'] == null) {
+                    return false;
+                }
+                node = node.children[c - 'a'];
+            }
+            return node.isEnd;
+        }
+
+        public boolean startsWith(String prefix) {
+            TrieNode node = root;
+            for (char c : prefix.toCharArray()) {
+                if (node.children[c - 'a'] == null) {
+                    return false;
+                }
+                node = node.children[c - 'a'];
+            }
+            return true;
+        }
+    }
+}
