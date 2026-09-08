9e66d675d1509631b9bd7e3286443a723f36266b
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-08 18:42:00 +0530

    feat(day26): solve LC 684 - Redundant Connection

---
+++ src/day26/a684.java
@@ -0,0 +1,29 @@
+package day26;
+
+public class a684 {
+    static class Solution {
+        public int[] findRedundantConnection(int[][] edges) {
+            int n = edges.length;
+            int[] parent = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                parent[i] = i;
+            }
+            for (int[] edge : edges) {
+                int rootU = find(parent, edge[0]);
+                int rootV = find(parent, edge[1]);
+                if (rootU == rootV) {
+                    return edge;
+                }
+                parent[rootU] = rootV;
+            }
+            return new int[0];
+        }
+
+        private int find(int[] parent, int i) {
+            if (parent[i] != i) {
+                parent[i] = find(parent, parent[i]);
+            }
+            return parent[i];
+        }
+    }
+}
