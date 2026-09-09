77c1b9679d818c84aa0618db32d1ac08cc00844e
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-09 18:42:00 +0530

    feat(day27): solve LC 70 - Climbing Stairs

---
+++ src/day27/a70.java
@@ -0,0 +1,19 @@
+package day27;
+
+public class a70 {
+    static class Solution {
+        public int climbStairs(int n) {
+            if (n <= 2) {
+                return n;
+            }
+            int first = 1;
+            int second = 2;
+            for (int i = 3; i <= n; i++) {
+                int third = first + second;
+                first = second;
+                second = third;
+            }
+            return second;
+        }
+    }
+}
