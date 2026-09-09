2e541145bb6daf6133d05548ca0972d9e01a0f52
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-09 19:36:00 +0530

    test(day27): add test cases and main runner for c198

--- src/day27/c198.java
+++ src/day27/c198.java
@@ -1,6 +1,11 @@
 package day27;
 
 public class c198 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(s.rob(nums));
+    }
     static class Solution {
         public int rob(int[] nums) {
             int rob1 = 0;
