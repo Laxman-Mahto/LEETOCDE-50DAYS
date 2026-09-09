70ffbc0e6dbfce519325875f2e29821c4c7cde77
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-09 19:04:00 +0530

    feat(day27): solve LC 746 - Min Cost Climbing Stairs

---
+++ src/day27/b746.java
@@ -0,0 +1,16 @@
+package day27;
+
+public class b746 {
+    static class Solution {
+        public int minCostClimbingStairs(int[] cost) {
+            int first = cost[0];
+            int second = cost[1];
+            for (int i = 2; i < cost.length; i++) {
+                int current = cost[i] + Math.min(first, second);
+                first = second;
+                second = current;
+            }
+            return Math.min(first, second);
+        }
+    }
+}
