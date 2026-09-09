53f7b3e4218d991d7709485acca5f5c4ab1d2d13
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-09 19:48:00 +0530

    feat(day27): solve LC 213 - House Robber II

---
+++ src/day27/d213.java
@@ -0,0 +1,23 @@
+package day27;
+
+public class d213 {
+    static class Solution {
+        public int rob(int[] nums) {
+            if (nums.length == 1) {
+                return nums[0];
+            }
+            return Math.max(robRange(nums, 0, nums.length - 2), robRange(nums, 1, nums.length - 1));
+        }
+
+        private int robRange(int[] nums, int start, int end) {
+            int rob1 = 0;
+            int rob2 = 0;
+            for (int i = start; i <= end; i++) {
+                int temp = Math.max(rob1 + nums[i], rob2);
+                rob1 = rob2;
+                rob2 = temp;
+            }
+            return rob2;
+        }
+    }
+}
