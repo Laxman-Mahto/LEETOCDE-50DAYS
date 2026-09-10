34c926bb88684771a09fe41c40568fadd5cf0f75
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-10 20:20:00 +0530

    test(day28): add test cases and main runner for e416

--- src/day28/e416.java
+++ src/day28/e416.java
@@ -1,6 +1,11 @@
 package day28;
 
 public class e416 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 5, 11, 5};
+        System.out.println(s.canPartition(nums));
+    }
     static class Solution {
         public boolean canPartition(int[] nums) {
             int total = 0;
