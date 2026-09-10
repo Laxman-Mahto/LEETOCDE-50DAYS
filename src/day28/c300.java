9da7a73b5a792b611e9c9861bd552e5db28fcb35
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-10 19:36:00 +0530

    test(day28): add test cases and main runner for c300

--- src/day28/c300.java
+++ src/day28/c300.java
@@ -5,6 +5,11 @@ import java.util.Collections;
 import java.util.List;
 
 public class c300 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println(s.lengthOfLIS(nums));
+    }
     static class Solution {
         public int lengthOfLIS(int[] nums) {
             List<Integer> tails = new ArrayList<>();
