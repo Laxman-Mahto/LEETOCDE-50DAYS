cd530371af664377a1943c95278223515dd8626d
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-10 19:14:00 +0530

    test(day28): add test cases and main runner for b518

--- src/day28/b518.java
+++ src/day28/b518.java
@@ -1,6 +1,11 @@
 package day28;
 
 public class b518 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] coins = {1, 2, 5};
+        System.out.println(s.change(5, coins));
+    }
     static class Solution {
         public int change(int amount, int[] coins) {
             int[] dp = new int[amount + 1];
