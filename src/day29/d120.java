14fdb3221c6ce57cfb9e3e76328b0eb72b194f38
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-11 19:48:00 +0530

    feat(day29): solve LC 120 - Triangle

---
+++ src/day29/d120.java
@@ -0,0 +1,19 @@
+package day29;
+
+import java.util.List;
+
+public class d120 {
+    static class Solution {
+        public int minimumTotal(List<List<Integer>> triangle) {
+            int n = triangle.size();
+            int[] dp = new int[n + 1];
+            for (int i = n - 1; i >= 0; i--) {
+                List<Integer> row = triangle.get(i);
+                for (int j = 0; j <= i; j++) {
+                    dp[j] = row.get(j) + Math.min(dp[j], dp[j + 1]);
+                }
+            }
+            return dp[0];
+        }
+    }
+}
