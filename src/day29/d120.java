bea89f06f8cab0950c9830a952d48de1b99e18a0
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-11 19:58:00 +0530

    test(day29): add test cases and main runner for d120

--- src/day29/d120.java
+++ src/day29/d120.java
@@ -3,6 +3,14 @@ package day29;
 import java.util.List;
 
 public class d120 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        java.util.List<java.util.List<Integer>> triangle = new java.util.ArrayList<>();
+        triangle.add(java.util.Arrays.asList(2));
+        triangle.add(java.util.Arrays.asList(3, 4));
+        triangle.add(java.util.Arrays.asList(6, 5, 7));
+        System.out.println(s.minimumTotal(triangle));
+    }
     static class Solution {
         public int minimumTotal(List<List<Integer>> triangle) {
             int n = triangle.size();
