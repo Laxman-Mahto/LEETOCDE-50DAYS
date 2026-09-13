646421caacf2e4e90b34cacda8e7cb0edcf6d0db
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-13 18:42:00 +0530

    feat(day31): solve LC 136 - Single Number

---
+++ src/day31/a136.java
@@ -0,0 +1,13 @@
+package day31;
+
+public class a136 {
+    static class Solution {
+        public int singleNumber(int[] nums) {
+            int result = 0;
+            for (int num : nums) {
+                result ^= num;
+            }
+            return result;
+        }
+    }
+}
