3b039d2fbfbaacb1a658bd12138be00ced684763
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-13 20:10:00 +0530

    feat(day31): solve LC 268 - Missing Number

---
+++ src/day31/e268.java
@@ -0,0 +1,13 @@
+package day31;
+
+public class e268 {
+    static class Solution {
+        public int missingNumber(int[] nums) {
+            int res = nums.length;
+            for (int i = 0; i < nums.length; i++) {
+                res ^= i ^ nums[i];
+            }
+            return res;
+        }
+    }
+}
