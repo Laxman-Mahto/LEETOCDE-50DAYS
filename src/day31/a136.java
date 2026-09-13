a6f76a4ffb323f8f8d46f1eb2f377014cfaeb034
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-13 18:52:00 +0530

    test(day31): add test cases and main runner for a136

--- src/day31/a136.java
+++ src/day31/a136.java
@@ -1,6 +1,11 @@
 package day31;
 
 public class a136 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {4, 1, 2, 1, 2};
+        System.out.println(s.singleNumber(nums));
+    }
     static class Solution {
         public int singleNumber(int[] nums) {
             int result = 0;
