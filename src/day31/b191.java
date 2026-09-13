b4011a7ea985a1d4f14b22e9d4e8b35aaa813458
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-13 19:14:00 +0530

    test(day31): add test cases and main runner for b191

--- src/day31/b191.java
+++ src/day31/b191.java
@@ -1,6 +1,10 @@
 package day31;
 
 public class b191 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.hammingWeight(11));
+    }
     static class Solution {
         public int hammingWeight(int n) {
             int count = 0;
