26dcccfc20324b3c03e9937ba289205d9211e950
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-13 20:20:00 +0530

    test(day31): add test cases and main runner for e268

--- src/day31/e268.java
+++ src/day31/e268.java
@@ -1,6 +1,11 @@
 package day31;
 
 public class e268 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {3, 0, 1};
+        System.out.println(s.missingNumber(nums));
+    }
     static class Solution {
         public int missingNumber(int[] nums) {
             int res = nums.length;
