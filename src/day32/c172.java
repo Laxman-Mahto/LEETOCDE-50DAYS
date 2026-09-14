6d13a0688f3d12d6c625fb65d1ba6a4c001771f8
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-14 19:26:00 +0530

    feat(day32): solve LC 172 - Factorial Trailing Zeroes

---
+++ src/day32/c172.java
@@ -0,0 +1,14 @@
+package day32;
+
+public class c172 {
+    static class Solution {
+        public int trailingZeroes(int n) {
+            int count = 0;
+            while (n > 0) {
+                count += n / 5;
+                n /= 5;
+            }
+            return count;
+        }
+    }
+}
