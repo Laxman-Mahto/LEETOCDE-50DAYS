9b11a4f1afa61d8af048ebd9a3a23a0f3f318d54
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-15 19:48:00 +0530

    feat(day33): solve LC 127 - Word Ladder

---
+++ src/day33/d127.java
@@ -0,0 +1,48 @@
+package day33;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class d127 {
+    static class Solution {
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            Set<String> dict = new HashSet<>(wordList);
+            if (!dict.contains(endWord)) {
+                return 0;
+            }
+            Queue<String> queue = new LinkedList<>();
+            queue.offer(beginWord);
+            int level = 1;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    String curr = queue.poll();
+                    char[] chars = curr.toCharArray();
+                    for (int j = 0; j < chars.length; j++) {
+                        char original = chars[j];
+                        for (char c = 'a'; c <= 'z'; c++) {
+                            if (c == original) {
+                                continue;
+                            }
+                            chars[j] = c;
+                            String next = new String(chars);
+                            if (next.equals(endWord)) {
+                                return level + 1;
+                            }
+                            if (dict.contains(next)) {
+                                dict.remove(next);
+                                queue.offer(next);
+                            }
+                        }
+                        chars[j] = original;
+                    }
+                }
+                level++;
+            }
+            return 0;
+        }
+    }
+}
