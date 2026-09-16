6af90df89415c16f1232cfb9dc5d46929eaecedc
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-16 18:52:00 +0530

    test(day34): add test cases and main runner for a42

--- src/day34/a42.java
+++ src/day34/a42.java
@@ -1,6 +1,11 @@
 package day34;
 
 public class a42 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println(s.trap(height));
+    }
     static class Solution {
         public int trap(int[] height) {
             int left = 0;
