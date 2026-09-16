18d53491d202c40fe73e55267c79b0374b40a936
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-16 20:20:00 +0530

    test(day34): add test cases and main runner for e295

--- src/day34/e295.java
+++ src/day34/e295.java
@@ -4,6 +4,14 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class e295 {
+    public static void main(String[] args) {
+        MedianFinder mf = new MedianFinder();
+        mf.addNum(1);
+        mf.addNum(2);
+        System.out.println(mf.findMedian());
+        mf.addNum(3);
+        System.out.println(mf.findMedian());
+    }
     static class MedianFinder {
         private PriorityQueue<Integer> small;
         private PriorityQueue<Integer> large;
