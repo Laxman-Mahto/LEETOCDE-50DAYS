ae71bb2ee9b7267dd44fe4823ef674fabdb4310b
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-17 18:52:00 +0530

    test(day35): add test cases and main runner for a51

--- src/day35/a51.java
+++ src/day35/a51.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class a51 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solveNQueens(4));
+    }
     static class Solution {
         public List<List<String>> solveNQueens(int n) {
             List<List<String>> result = new ArrayList<>();
