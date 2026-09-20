bd21ac16cfb5adc2f5379fde75d4c9e2ed49b260
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-20 23:57:36 +0530

    day36

---
+++ src/day36/a1401.java
@@ -0,0 +1,16 @@
+package day36;
+
+public class a1401 {
+    public static int reverseDegree(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int value = 'z' - s.charAt(i) + 1;
+            sum += value * (i + 1);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseDegree("abc"));
+    }
+}
