87d4cc7c9a0461ba4d11122546b89bdb966a4d3a
Laxman-Mahto <laxmanmahto003@gmail.com>
2026-09-22 23:33:12 +0530

    day37

---
+++ src/day37/a744.java
@@ -0,0 +1,27 @@
+package day37;
+
+public class a744 {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return letters[left % letters.length];
+    }
+
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+
+        System.out.println(nextGreatestLetter(letters, target));
+    }
+}
